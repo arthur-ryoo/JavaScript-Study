c868b8e3f9b2de8da4ac8c442bfdc8226e7b98d1
arthur-ryoo <jene0523@gmail.com>
2020-09-04 23:07:12 -0700

    Study default and rest parameters and local scope

--- js/function.js
+++ js/function.js
@@ -32,3 +32,38 @@ function changeName(obj) {
 const ellie = { name: 'ellie' };
 changeName(ellie);
 console.log(ellie);
+
+// 3. Default parameters (added in ES6)
+// It will display 'unknown if there is no parameter on from
+function showMessage(message, from = 'unknown') {
+    console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+    // or
+    for (const arg of args) {
+        console.log(arg);
+    }
+    // or
+    args.forEach((arg) => console.log(arg));
+}
+printAll('dream', 'coding', 'ellie');
+
+// 5. Local scope
+let globalMessage = 'global'; // global variable
+function printMessage() {
+    let message = 'hello';
+    console.log(message); // local variable
+    console.log(globalMessage);
+    function printAnother() {
+        console.log(message);
+        let childMessage = 'hello';
+    }
+    console.log(childMessage); // It occurs an error
+}
+printMessage();
