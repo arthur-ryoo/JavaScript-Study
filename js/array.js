b82fd3203d2985cfe4ef981e101195d56453c7e9
arthur-ryoo <jene0523@gmail.com>
2020-09-07 23:11:24 -0700

    Study how to add, delete, copy on array

--- js/array.js
+++ js/array.js
@@ -31,3 +31,40 @@ for (const fruit of fruits) {
 
 // c. forEach
 fruits.forEach((fruit) => console.log(fruit));
+
+// 4. Addition, deletion, copy
+// push: add an item to the end
+fruits.push('berry', 'peach');
+console.log(fruits);
+
+// pop: remove an item from the end
+fruits.pop();
+fruits.pop();
+console.log(fruits);
+
+// unshift: add an item to the beginning
+fruits.unshift('berry', 'lemon');
+console.log(fruits);
+
+// shift: remove an item from the beginning
+fruits.shift();
+fruits.shift();
+console.log(fruits);
+
+// note!! shift and unshift are slower than pop and push.
+// It's better to use pop and push instead of shift and unshift
+
+// splice: remove an item by index position
+fruits.push('berry', 'peach', 'lemon');
+console.log(fruits);
+// fruits.splice(1); // it deletes every item expect the first item in the array
+fruits.splice(1, 1);
+console.log(fruits);
+fruits.splice(1, 1, 'green apple', 'watermelon'); // it replaces berry and peach with green apple and watermelon
+console.log(fruits);
+
+// combine two arrays
+const fruits2 = ['pear', 'coconut'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
