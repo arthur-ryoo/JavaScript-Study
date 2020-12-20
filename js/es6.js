5f7e0f3021d22bbb70abb25f9d54304bb7d5aa06
ArthurRyoo <jene0523@gmail.com>
2020-12-19 16:45:11 -0800

    Learn spread syntax

--- js/es6.js
+++ js/es6.js
@@ -64,3 +64,35 @@
     }
 
 }
+
+
+// * Spread syntax
+// If you change the value of the original array, the copied arrays will have the changed value as well.
+{
+    const obj1 = { key: 'key1' };
+    const obj2 = { key: 'key2' };
+    const array = [obj1, obj2];
+
+    // array copy
+    const arrayCopy = [...array];
+    console.log(array, arrayCopy);
+
+    const arrayCopy2 = [...array, { key: 'key3' }];
+    console.log(arrayCopy2);
+
+    // object copy 
+    const obj3 = { ...obj1 };
+    console.log(obj3);
+
+    // array concatenation
+    const fruits1 = ['a', 'b'];
+    const fruits2 = ['c', 'd'];
+    const fruits = [...fruits1, ...fruits2];
+    console.log(fruits);
+
+    // object merge
+    const dog1 = { dog1: 'puppy' };
+    const dog2 = { dog2: 'dog' };
+    const dog = { ...dog1, ...dog2 };
+    console.log(dog);
+}
