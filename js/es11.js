a45ef0d3622d39cb6372cf0ef49d96add7fe6f83
ArthurRyoo <jene0523@gmail.com>
2020-12-22 22:01:02 -0800

    Learn nullish coalescing operator

--- js/es11.js
+++ js/es11.js
@@ -24,3 +24,47 @@
         printManager(person2);
     }
 }
+
+// * Nullish coalescing operator (ES11)
+
+{
+    // Logical OR operator
+    // false: false, '', 0, null, undefined
+    {
+        const name = 'Arthur';
+        const userName = name || 'Guest';
+        console.log(userName); // Arthur
+    }
+
+    {
+        const name = null;
+        const userName = name || 'Guest';
+        console.log(userName); // Guest
+    }
+
+    // bad
+    {
+        const name = '';
+        const userName = name || 'Guest';
+        console.log(userName); // Guest
+    }
+
+    {
+        const num = 0;
+        const message = num || 'undefined';
+        console.log(message); // undefined
+    }
+
+    // good
+    // If the name doesn't have any values, use 'Guest'
+    {
+        const name = '';
+        const userName = name ?? 'Guest';
+        console.log(userName);
+
+        // If the num doesn't have a number, use 'undefined'
+        const num = 0;
+        const message = num ?? 'undefined';
+        console.log(message);
+    }
+}
